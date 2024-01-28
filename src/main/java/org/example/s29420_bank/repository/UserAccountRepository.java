7e091a8a5764f1de8891d2280bc13f315debb3e5
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-01-28 16:00:05 +0100

    Added autoincrement UserAccount id

--- src/main/java/org/example/s29420_bank/repository/UserAccountRepository.java
+++ src/main/java/org/example/s29420_bank/repository/UserAccountRepository.java
@@ -1,6 +1,5 @@
 package org.example.s29420_bank.repository;
 
-import org.apache.catalina.User;
 import org.example.s29420_bank.model.UserAccount;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +12,7 @@ public class UserAccountRepository {
     List<UserAccount> userAccountsList = new ArrayList<>();
 
     public UserAccount add(UserAccount userAccount) {
+        userAccount.setId(userAccountsList.size() + 1);
         userAccountsList.add(userAccount);
         return userAccount;
     }
