9cec74211f8b5076cc5b91dfc02abb654b1f4d13
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-01-28 15:23:16 +0100

    Created ValidationException

---
+++ src/main/java/org/example/s29420_bank/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package org.example.s29420_bank.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
